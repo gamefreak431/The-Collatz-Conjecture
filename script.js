92f31c2de2afc65e067ef15ac734609e4a50d120
John McLeish <mcleish.john@gmail.com>
2025-09-14 17:27:16 +1000

    de-nested the error handling for invalid inputs into it's own check

--- script.js
+++ script.js
@@ -29,26 +29,25 @@ function collatz(){
     output.innerHTML = ""; //clears the output for the next number, assuming the user is trying another number.
     output.id = "output"; //Resets the output id to output in case it was changed to error from a previous invalid input.
     let stepsToOne = 1; //Counts the steps to reach 1. Starts at one as the user input is the first step.
-    num % 2 == 0 ? output.appendChild(elementPrep(num, "even", null)) : output.appendChild(elementPrep(num, "odd", null)); //Displays the user input number in the correct colour.
-    if(num > 0){
-        while (num != 1){ //If you don't have the loop break when it reaches 1, it will infinitely loop through 1, 4, 2, 1.
-            if(num % 2 == 0) {
-                num = num / 2;
-                output.appendChild(elementPrep(num, "even", null));
-            }
-            else {
-                num = num * 3 + 1;
-                output.appendChild(elementPrep(num, "odd", null));
-            }
-            stepsToOne++;
-        }
-        steps(stepsToOne);
-    }
-    else {
+    if (num < 1) {
         steps(0); //Sets the steps to 0 if the input is invalid.
         output.id = "error"; //Changes the output id to error to apply error styling.
-        output.innerHTML = "Please enter a postive number <br> or a number other than 0"; //Error message for negative numbers, 0, or no input
+        output.innerHTML = "Please enter a postive number <br> or a number other than 0"; //Error message for negative numbers, 0
+        return; //Stops the function from continuing.
+    };
+    num % 2 == 0 ? output.appendChild(elementPrep(num, "even", null)) : output.appendChild(elementPrep(num, "odd", null)); //Displays the user input number in the correct colour.
+    while (num != 1){ //If you don't have the loop break when it reaches 1, it will infinitely loop through 1, 4, 2, 1.
+        if(num % 2 == 0) {
+            num = num / 2;
+            output.appendChild(elementPrep(num, "even", null));
+        }
+        else {
+            num = num * 3 + 1;
+            output.appendChild(elementPrep(num, "odd", null));
+        }
+        stepsToOne++;
     }
+    steps(stepsToOne);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
